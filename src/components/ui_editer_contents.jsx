5c3ae3660fadd4c02aa080e0e357856483076a48
kuwadayo <sasafukoharu@gmail.com>
2025-06-22 23:19:29 +0900

    htmlのシンタックスハイライト用のカラーコード指定完了

--- src/components/ui_editer_contents.jsx
+++ src/components/ui_editer_contents.jsx
@@ -1,18 +1,35 @@
+//html風の内容
 export function Box1Content() {
-  return <div>📦 Box 1 の詳細内容です</div>;
+  return (
+    <>
+      <div className="text-xs font-mono">
+        <div>
+          {"<"}!DOCTYPE HTML{">"}
+        </div>
+        <div>
+          {"<"}meta chrset="UTF-8 /"{">"}
+        </div>
+      </div>
+    </>
+  );
 }
 
+//css風の内容
 export function Box2Content() {
   return <div>📦 Box 2 の詳細内容です</div>;
 }
 
+//js風の内容
 export function Box3Content() {
   return <div>📦 Box 3 の詳細内容です</div>;
 }
+
+//json風の内容
 export function Box4Content() {
   return <div>📦 Box 4 の詳細内容です</div>;
 }
 
+//md風の内容
 export function Box5Content() {
   return <div>📦 Box 5 の詳細内容です</div>;
 }
